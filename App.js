2367f7c2c327a9e27ed4aed891636e8ab005d24a
CRIMSON <olueatoworosanwo@gmail.com>
2022-02-18 09:40:59 +0100

    List Item done

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import { StyleSheet, Text, View } from "react-native";
+import List from "./screens/List";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NativeBaseProvider>
+            <View style={styles.container}>
+                <StatusBar style="auto" />
+                <List />
+            </View>
+        </NativeBaseProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        paddingTop: 0,
+    },
 });
--- screens/List/index.js
+++ screens/List/index.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Box, Center, FlatList, Pressable } from "native-base";
+import { Box, Center, FlatList, HStack, Image, Pressable, Text, VStack } from "native-base";
 import shortid from "shortid";
-const WinWidth = Dimensions.get("window");
+import { Dimensions, TouchableOpacity } from "react-native";
+const WinWidth = Dimensions.get("window").width;
+const CARD_WIDTH = 250;
 const CARDS = [
     {
         id: shortid.generate(),
@@ -36,56 +38,47 @@ const CARDS = [
 ];
 
 const ICONS = [
-    {
-        img: require("../../assets/coin_logos/bitcoin.png"),
-    },
-    {
-        img: require("../../assets/coin_logos/bnbpng.png"),
-    },
-    {
-        img: require("../../assets/coin_logos/dogepng.png"),
-    },
-    {
-        img: require("../../assets/coin_logos/ethereumpng.png"),
-    },
-    {
-        img: require("../../assets/coin_logos/litecoinpng.png"),
-    },
-    {
-        img: require("../../assets/coin_logos/polygonpng.png"),
-    },
-    {
-        img: require("../../assets/coin_logos/solanapng.png"),
-    },
-    {
-        img: require("../../assets/coin_logos/tron.png"),
-    },
-    {
-        img: require("../../assets/coin_logos/xrppng.png"),
-    },
+    require("../../assets/coin_logos/bitcoin.png"),
+    require("../../assets/coin_logos/bnbpng.png"),
+    require("../../assets/coin_logos/dogepng.png"),
+    require("../../assets/coin_logos/ethereumpng.png"),
+    require("../../assets/coin_logos/litecoinpng.png"),
+    require("../../assets/coin_logos/polygonpng.png"),
+    require("../../assets/coin_logos/solanapng.png"),
+    require("../../assets/coin_logos/tron.png"),
 ];
 
 const List = () => {
     return (
         <Box flex={1}>
-            <VStack flex={1} justifyContent="space-between">
+            <VStack flex={1} justifyContent="space-around">
                 <FlatList
                     data={CARDS}
                     keyExtractor={({ id }) => id}
                     renderItem={({ item }) => (
-                        <Box bg={item.color} p={"5"} w={WinWidth * 0.7} h={200}>
+                        <Box bg={item.color} p={"5"} w={CARD_WIDTH} h={200} rounded="3xl" m={5}>
                             <Text>{item.title}</Text>
                         </Box>
                     )}
+                    horizontal
+                    snapToInterval={CARD_WIDTH + 40}
+                    showsHorizontalScrollIndicator={false}
                 />
-                <Box flexWrap={"wrap"}>
-                    {ICONS.map(({ img }, index) => (
-                        <Pressable key={index}>
-                            <Center size={30}>
-                                <Image source={img} />
-                            </Center>
-                        </Pressable>
-                    ))}
+                <Box p="5" justifyContent={"center"}>
+                    <HStack flexWrap={"wrap"} justifyContent="space-between">
+                        {ICONS.map((img, index) => (
+                            <TouchableOpacity key={index}>
+                                <Center size={30} rounded={"full"} m="5">
+                                    <Image
+                                        source={img}
+                                        alt={`${img}`}
+                                        style={{ width: "100%", height: "100%" }}
+                                        resizeMode="cover"
+                                    />
+                                </Center>
+                            </TouchableOpacity>
+                        ))}
+                    </HStack>
                 </Box>
             </VStack>
         </Box>
