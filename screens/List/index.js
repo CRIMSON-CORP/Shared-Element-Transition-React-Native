2c7d637b9d9fb6ed757d06eecbdf9c71352a5e31
CRIMSON <olueatoworosanwo@gmail.com>
2022-02-18 08:58:54 +0100

    creating list component

--- screens/List/index.js
+++ screens/List/index.js
@@ -1,29 +1,36 @@
-import { View, Text } from "react-native";
 import React from "react";
-
+import { Box, Center, FlatList, Pressable } from "native-base";
+import shortid from "shortid";
+const WinWidth = Dimensions.get("window");
 const CARDS = [
     {
+        id: shortid.generate(),
         title: "Card One",
         color: "red.300",
     },
     {
+        id: shortid.generate(),
         title: "Card Two",
         color: "green.300",
     },
     {
-        title: "Card One",
+        id: shortid.generate(),
+        title: "Card Three",
         color: "yellow.300",
     },
     {
-        title: "Card One",
+        id: shortid.generate(),
+        title: "Card Four",
         color: "orange.300",
     },
     {
-        title: "Card One",
+        id: shortid.generate(),
+        title: "Card Five",
         color: "violet.300",
     },
     {
-        title: "Card One",
+        id: shortid.generate(),
+        title: "Card Six",
         color: "amber.300",
     },
 ];
@@ -32,13 +39,56 @@ const ICONS = [
     {
         img: require("../../assets/coin_logos/bitcoin.png"),
     },
+    {
+        img: require("../../assets/coin_logos/bnbpng.png"),
+    },
+    {
+        img: require("../../assets/coin_logos/dogepng.png"),
+    },
+    {
+        img: require("../../assets/coin_logos/ethereumpng.png"),
+    },
+    {
+        img: require("../../assets/coin_logos/litecoinpng.png"),
+    },
+    {
+        img: require("../../assets/coin_logos/polygonpng.png"),
+    },
+    {
+        img: require("../../assets/coin_logos/solanapng.png"),
+    },
+    {
+        img: require("../../assets/coin_logos/tron.png"),
+    },
+    {
+        img: require("../../assets/coin_logos/xrppng.png"),
+    },
 ];
 
 const List = () => {
     return (
-        <View>
-            <Text>List</Text>
-        </View>
+        <Box flex={1}>
+            <VStack flex={1} justifyContent="space-between">
+                <FlatList
+                    data={CARDS}
+                    keyExtractor={({ id }) => id}
+                    renderItem={({ item }) => (
+                        <Box bg={item.color} p={"5"} w={WinWidth * 0.7} h={200}>
+                            <Text>{item.title}</Text>
+                        </Box>
+                    )}
+                />
+                <Box flexWrap={"wrap"}>
+                    {ICONS.map(({ img }, index) => (
+                        <Pressable key={index}>
+                            <Center size={30}>
+                                <Image source={img} />
+                            </Center>
+                        </Pressable>
+                    ))}
+                </Box>
+            </VStack>
+        </Box>
     );
 };
 
